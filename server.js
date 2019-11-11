15a0826f0f326dedfd3af2a67d41780b01b70a6f
kim <s158614@nishitech.ac.jp>
2019-11-11 23:50:24 +0900

    AddMongooseDB
    MongoDB와 server연동을 위한 mongoose 라이브러리 추가 및 설정

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 const port = 1234;
 
@@ -9,6 +10,12 @@ const productsRoutes = require("./routes/api/products");
 const orderRoutes = require("./routes/api/orders");
 
 app.listen(port, console.log("서버 시작"));
+
+const dbaddress = "mongodb+srv://prodix:aa1234@cluster0-nbev1.mongodb.net/test?retryWrites=true&w=majority" ;
+mongoose.connect(dbaddress, {useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => console.log("몽고DB에 연결되었습니다."))
+    .catch(err => console.log(err.message));
+
 
 //라우터 아래에다가 bodyParser 를 사용할시 오류 발생 이유는?
 app.use(bodyParser.json());
