3a88f741e4060dea4e84d329c4314a65a0e362b6
kim <s158614@nishitech.ac.jp>
2019-11-17 21:04:29 +0900

    Add user model&CR

---
+++ models/users.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema({
+    id : {
+        type : String,
+        required : true
+    },
+    password : {
+        type : {Number, String},
+        required : true
+    },
+    createdAt : {
+        type : Date,
+        default : Date.now
+    }
+});
+
+
+module.exports = mongoose.model("user", userSchema);
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,27 +1,59 @@
 const express = require("express");
 const router = express.Router();
+const userModel = require("../../models/users");
+
+
 
 //내 정보 불러오기 get
 router.get("/", (req, res) => {
-    res.json({
-        msg : "내 정보"
-    })
+
+    userModel
+        .find()
+        .exec()
+        .then(result => {
+            res.status(200).json({
+                msg : "성공적으로 마이페이지를 불러왔습니다.",
+                userInfo : result
+            });
+        })
+        .catch(err => {
+            res.status(500).json({
+                error : err.message
+            });
+        });
 });
+
+
+
 
 //내 정보 등록하기 post
 router.post("/", (req, res) => {
 
-    const user = {
-        email : req.body.email,
+    const user = new userModel({
+        id : req.body.id,
         password : req.body.password
-    };
+    });
 
-    res.json({
-        msg : "나의 정보가 등록되었습니다.",
-        userinfo : user
+    user
+        .save()
+        .then(result => {
+            console.log(result);
+            res.status(200).json({
+                msg : "성공적으로 회원가입을 완료했습니다.",
+                userInfo : result
+            })
+        })
+        .catch(err => {
+            res.status(500).json({
+                error : err.message
+            });
+        });
 
-    })
 });
+
+
+
+
 
 //내 정보 수정하기 patch or put
 router.patch("/", (req, res) => {
@@ -29,6 +61,10 @@ router.patch("/", (req, res) => {
         msg : "나의 정보를 수정합니다."
     })
 });
+
+
+
+
 
 //내 정보 삭제하기 delete
 router.delete("/", (req, res) => {
